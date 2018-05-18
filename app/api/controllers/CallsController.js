24b7d9b9ff1945706dd428715d94effdb2c6a0cc
Navin Sasikumar <navinsasikumar@gmail.com>
2018-05-18 18:06:10 -0400

    More calls. Show Spectrpgram. Keyboard shortcuts.

--- app/api/controllers/CallsController.js
+++ app/api/controllers/CallsController.js
@@ -5,16 +5,23 @@
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
 
+var jsmediatags = require("jsmediatags");
+var btoa = require('btoa');
+
+var returnView = function(res, call) {
+  return res.view('test', {
+    call: call
+  });
+};
+
 module.exports = {
   random: function(req, res, next) {
     Calls.count().exec(function(err, count) {
-      sails.log.debug('Count: ' + count);
       if (count === 0) {
         sails.log.error('No calls found');
         return res.status(404).send('No calls found');
       }
       var id = Math.floor(Math.random() * Math.floor(count)) + 1;
-      sails.log.debug(id);
 
       Calls.findOne({id: id}).exec(function(err, call) {
         if (err) {
@@ -25,8 +32,25 @@ module.exports = {
           return res.status(404).send('No calls found');
         }
         //return res.send(call.name);
-        return res.view('test', {
-          call: call
+        jsmediatags.read(__dirname + '/../../assets/' + call.path, {
+          onSuccess: function(tag) {
+            var image = tag.tags.picture;
+            if (image) {
+              var base64String = "";
+              for (var i = 0; i < image.data.length; i++) {
+                base64String += String.fromCharCode(image.data[i]);
+              }
+              var base64 = "data:image/jpeg;base64," + btoa(base64String);
+              call.image = base64;
+              return returnView(res, call);
+            } else {
+              return returnView(res, call);
+            }
+          },
+          onError: function(error) {
+            console.log(':(', error.type, error.info);
+            return returnView(res, call);
+          }
         });
       });
     });
